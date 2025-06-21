74a7d91594f594446097b8f1002d0a46ce99f9b7
Enrique Perez II <116796331+TX-Biskit@users.noreply.github.com>
2025-06-21 18:19:29 -0500

    Add files via upload

---
+++ soundboard.js
@@ -0,0 +1,25 @@
+function playSoundFootsteps(Footsteps){
+
+    var audio = new Audio('sounds/Footsteps.mp3');
+    audio.play();
+}
+
+function playSoundApplause(Applause){
+    var audio = new Audio('sounds/applause.mp3');
+    audio.play();
+}
+
+function playSoundBeeps(Beep){
+    var audio = new Audio('sounds/beep.mp3');
+    audio.play();
+}
+
+function playSoundThud(Thud){
+    var audio = new Audio('sounds/thud.mp3');
+    audio.play();
+}
+
+function playSoundRecordScratch(RecordScratch){
+    var audio = new Audio('sounds/record-scratch.mp3');
+    audio.play();
+}
